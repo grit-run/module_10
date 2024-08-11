dd6e66a8acd06df39677d9e2f7d0b71fabdc3674
elinsky <dmitry.ilinsky@gmail.com>
2024-08-11 11:44:07 +0300

    still quite long tie to execute.threads twice are faster

--- module_10_1.py
+++ module_10_1.py
@@ -11,14 +11,22 @@ def write_words(word_count, file_name):
             file.write("Какое-то слово № {}".format(i) + '\n')
             sleep(0.5)
             from datetime import datetime
-    time_end = datetime.now()
-    print(time_end - time_start)
+    print("Завершилась запись в файл", file_name)
 
 
-thread1 = Thread(target=write_words, args=(10, "example1.txt"))
-thread2 = Thread(target=write_words, args=(30, "example2.txt"))
-thread3 = Thread(target=write_words, args=(200, "example3.txt"))
-thread4 = Thread(target=write_words, args=(100, "example4.txt"))
+write_words(10, "example1.txt")
+write_words(30, "example2.txt")
+write_words(200, "example3.txt")
+write_words(100, "example4.txt")
+
+time_end = datetime.now()
+print(time_end - time_start)
+
+time_start2 = datetime.now()
+thread1 = Thread(target=write_words, args=(10, "example5.txt"))
+thread2 = Thread(target=write_words, args=(30, "example6.txt"))
+thread3 = Thread(target=write_words, args=(200, "example7.txt"))
+thread4 = Thread(target=write_words, args=(100, "example8.txt"))
 
 thread1.start()
 thread2.start()
@@ -30,7 +38,5 @@ thread2.join()
 thread3.join()
 thread4.join()
 
-write_words(10, "example1.txt")
-write_words(30, "example2.txt")
-write_words(200, "example3.txt")
-write_words(100, "example4.txt")
+time_end2 = datetime.now()
+print(time_end2 - time_start2)
