ddde7ab7578fe0b3fdc79b5e7b47c0a855977252
elinsky <dmitry.ilinsky@gmail.com>
2024-08-11 20:14:57 +0300

    Bit simple, faster

--- module_10_5.py
+++ module_10_5.py
@@ -1,21 +1,32 @@
+import multiprocessing
 from datetime import datetime
 
 filenames = ["./file {number}.txt".format(number=number) for number in range(1, 5)]
-print(filenames)
 
 
 def read_info(name):
     all_data = []
     with open(name, 'r', encoding='utf-8') as file:
-        row = file.readline()
-        while row:
-            all_data.append(row)
-            row = file.readline()
+        while file.readline():
+            all_data.append(file.readline())
 
-start = datetime.now()
 
-for filename in filenames:
-    read_info(filename)
+#start = datetime.now()
+#
+#for filename in filenames:
+#    read_info(filename)
+#
+#end = datetime.now()
+#print(end - start)
 
-end = datetime.now()
-print(end - start)
+if __name__ == "__main__":
+    with multiprocessing.Pool(processes=4) as pool:
+        start1 = datetime.now()
+        pool.map(read_info, filenames)
+
+    end1 = datetime.now()
+    print(end1 - start1)
+
+# Линейный вызов 0:00:01.767815
+
+# Многопроцессный 0:00:00.611422
